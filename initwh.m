5904fe2fe43309ab2007e5884579514e24474c00
Ondrej Mandula <aludnam@gmail.com>
2013-01-12 15:29:24 +0100

    Modified initialisation of h in inintwh.m such that the initial valuesa are small. It looks like it works better because the new components (random) are always taken with very small weights - therefore it enhances the already estimated components. In the old version h was initialised wiht much lower numbers and it seems to perform better.

--- initwh.m
+++ initwh.m
@@ -4,8 +4,8 @@ function [winit,hinit]=initwh(N,T,K,meanv,bg)
 % bg - background value per pixel
 % random intitialisation but keeping the mean(mean(winit*hinit)) equal to meanv
 
-winit = 2/N*max(rand(N,K),eps); % sum(winit,1) is approximately 1
-hinit = N*T*abs(meanv-bg)*(2/T)/(K-1)*max(rand(K,T),eps); % mean(mean(winit*hinit))=meanv; abs() is a cheap fix for case wehre meanv<bg
-
+winit = normL(2/N*max(rand(N,K),eps),1); % sum(winit,1) is approximately 1
+%hinit = N*T*abs(meanv-bg)*(2/T)/(K-1)*max(rand(K,T),eps); % mean(mean(winit*hinit))=meanv; abs() is a cheap fix for case wehre meanv<bg
+hinit=max(rand(K,T),eps);
 winit(:,K)=1/N*ones(N,1); % flat background component
 hinit(K,:)=bg*N*ones(1,T); % intensity of the background
