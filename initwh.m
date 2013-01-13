2018245d7820109696a7ef8f7df187c66c1a78af
Ondrej Mandula <aludnam@gmail.com>
2013-01-13 17:34:28 +0100

    Minor change in the initwh.m file.

--- initwh.m
+++ initwh.m
@@ -6,6 +6,6 @@ function [winit,hinit]=initwh(N,T,K,meanv,bg)
 
 winit = normL(2/N*max(rand(N,K),eps),1); % sum(winit,1) is 1
 %hinit = N*T*abs(meanv-bg)*(2/T)/(K-1)*max(rand(K,T),eps); % mean(mean(winit*hinit))=meanv; abs() is a cheap fix for case wehre meanv<bg
-hinit=(meanv-bg)*max(rand(K,T),eps); % The multiplication by (meanv-bg) is there for getting it into reasonable range.
+hinit=max((meanv-bg)*rand(K,T),eps); % The multiplication by (meanv-bg) is there for getting it into reasonable range.
 winit(:,K)=1/N*ones(N,1); % flat background component
 hinit(K,:)=bg*N*ones(1,T); % intensity of the background
