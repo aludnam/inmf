5090c8be4c786c12c2a26ef9749f0612292ec133
Ondrej Mandula <aludnam@gmail.com>
2013-04-23 00:06:27 +0200

    Changed stitching for 'smooth' version - weighting overlapping areas with weights proportional to the distance from the edge of the patch.

--- stitch.m
+++ stitch.m
@@ -1,6 +1,17 @@
 function resIm=stitch(rf,pow)
-%pow=20;
-%rf=4; % should be power of 2
+% resIm=stitch(rf,pow)
+% Makes high-resolution image from iNMF estimated soureces. Stitches results from iNMF 
+% evaluation of different patches. 
+%
+% resIm - Visualisation of the iNMF results.
+% rf - Resample factor (default rf=4).
+% pow - Power to which the resampled individual soureces should be rased (default pow=10).
+
+if ~exist('rf','var'); rf=4; end
+if ~exist('pow','var'); pow=10; end
+
+fprintf('Resampling the results %g times.\n',rf)
+fprintf('Taking the estimated sources to the power of  %g.\n',pow)
 
 d=dir('P*');
 [NW,SE]=resSize(d);
@@ -11,27 +22,28 @@ for ii=1:length(d)
     fprintf('Processing patch %s\n', d(ii).name)
     sd = dir([d(ii).name '/results_run*']);
     n=0;
-    for jj=1:length(sd)                
+    out=[];
+    for jj=1:length(sd)
         try
             load ([d(ii).name '/' sd(jj).name '/w'])
             load ([d(ii).name '/' sd(jj).name '/h'])
             load ([d(ii).name '/' sd(jj).name '/peval'])
             [outtmp,nxi,nyi]=makeHiRes(w,h,peval.nx,peval.ny,rf,pow);
+            wxy=makeWindow(size(outtmp),rf*peval.patchOverlap);
             if n==0
                 out=zeros(nxi,nyi);
-            end            
-            out = out+outtmp;
-            n=n+1;        
-        end        
+            end
+            out = out+wxy.*outtmp;
+            n=n+1;
+        end
     end
-    out=out/max(n,1); % to get mean value of the evaluations
+    out=out/max(n,1);                   % get the mean value of the evaluations
     
-    cTL=rf*(peval.cornerNW-1)+1;        % counts stats from 1
-    
-    cTLloc=[1,1]*rf*peval.patchOverlap/2+1;   % coordinates in the patch
-    cBRloc=size(out)-rf*peval.patchOverlap/2;     % coordinates in the patch
-    sloc=cBRloc-cTLloc;                         % size of the patch with removed half of the overlap on each side    
-    resIm(cTL(1):cTL(1)+sloc(1),cTL(2):cTL(2)+sloc(2))=out(cTLloc(1):cBRloc(1),cTLloc(2):cBRloc(2));
+    cTL=rf*(peval.cornerNW-1)+1;        % counts starts from 1
+            
+    c1=cTL(1):cTL(1)+size(out,1)-1;
+    c2=cTL(2):cTL(2)+size(out,2)-1;
+    resIm(c1,c2)=resIm(c1,c2)+out;    
 end
 end
 
@@ -44,6 +56,20 @@ for ii=1:length(d)
     cNW=min(cNW,peval.cornerNW);
     cSE=max(cSE,peval.cornerSE);
 end
-cNW=cNW-peval.patchOverlap/2;
-cSE=cSE-peval.patchOverlap/2;
+end
+
+function wxy=makeWindow(s,overlapIm)
+ov=overlapIm-1; % overlap starts from zero (removing the border pixels due to possible artefacts)
+
+[x,y]=meshgrid(1:s(1),1:s(2));
+
+mx1=x<=ov;
+mx2=(s(1)-x)<ov;
+wx=1/ov*((x-1).*mx1+(s(1)-x).*mx2)+~(mx1+mx2);
+
+my1=y<=ov;
+my2=(s(1)-y)<ov;
+wy=1/ov*((y-1).*my1+(s(1)-y).*my2)+~(my1+my2);
+
+wxy=wx.*wy;
 end
