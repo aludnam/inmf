a975dacdbc9987586daa16ca5e2c12c5d46d3fb2
Ondrej Mandula <aludnam@gmail.com>
2013-05-02 10:55:03 +0200

    Cleaning...

--- inmf_main.m
+++ inmf_main.m
@@ -86,7 +86,12 @@ for indexRun=peval.runs
         end
     end
 end
-end
+end % of main function
+
+%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%
+
+% Nested functions:
 
 function printmsg(patchX,patchY,peval)
 
@@ -95,4 +100,41 @@ fprintf('Top-left corner of the patch [%g %g]\n',peval.cornerNW);
 fprintf('Bottom-right corner of the patch [%g %g]\n',peval.cornerSE);
 fprintf('Patch size is [%g %g] pixels.\n',peval.nx, peval.ny);
 fprintf('Number of sources: %g\n',peval.K);
+end
+
+function savedata(path,w,h,peval)
+% Saves iNMF results into a direcotry defined by 'path'.
+%
+% savedata(path,w,h,peval)
+
+if ~exist('path','var')
+    path=cd; % Default direcotry.
+    peval.path_results = path;
+end 
+
+fprintf('Results saved in : %s\n',path);
+mkdir(path)
+
+save([peval.path_results '/w'],'w');
+save([peval.path_results '/h'],'h');
+save([peval.path_results '/peval'],'peval');
+
+% Save peval as a peval.txt file. 
+fid = fopen([peval.path_results '/peval.txt'],'wt');
+fn =fieldnames(peval);
+for ii = 1: length(fn)
+    name = fn{ii};
+    value = getfield(peval,fn{ii});
+    if ischar (value)
+        fprintf (fid, [name ' = ''' value '''\n']);
+    else
+        if length(value)==1
+            fprintf (fid, [name ' = %g \n'], value);
+        else
+            fprintf (fid, [name ' = [']);
+            fprintf (fid, '%g ', value);
+            fprintf (fid, ']\n');            
+        end
+    end
+end    
 end
